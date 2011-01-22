456ef96bb2b215b1e7ec12376d216b04260de789
Nick Freear <nfreear@yahoo.co.uk>
2011-01-22 00:28:27 +0000

    Minor tweaks to comments.

--- lang/en_utf8/filter_timelinewidget.php
+++ lang/en_utf8/filter_timelinewidget.php
@@ -5,6 +5,8 @@
  * @package   filter_timelinewidget
  * @copyright (c) 2010 Nicholas Freear {@link http://freear.org.uk}.
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v2 or later
+ *
+ * Moodle 1.9.x compatible.
  */
 global $CFG;
 require_once $CFG->dirroot
